059252622d6df691f8d6421b5f34c27a361f8004
meeeeeesteeeeso <marconsonni2007@gmail.com>
2024-11-26 12:32:18 +0100

    implemented the attack turn
    now player 2 (dummy2) is able to attack the ships of player 1.
    now player 2 can't see the cells in the enemy map without discovering them first by attacking

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -5,16 +5,17 @@ public class Giocatore {
     private int numNavi;
     public Nave[] navi;
 
-
+    public String getNick() {
+        return nick;
+    }
 
     public Giocatore(String nick, int numNavi) {
         this.nick = nick;
         this.numNavi = numNavi;
         this.navi = new Nave[numNavi];
-
     }
 
-    public void inizializzaNavi(){
+    public void inizializzaNavi() {
         for (int i = 0; i < this.numNavi; i++) {
             navi[i] = new Nave(i + 2);
         }
@@ -22,6 +23,7 @@ public class Giocatore {
 
     public int getNumNavi() {
         return numNavi;
+    }
 
     public void setNumNavi(int numNavi) {
         this.numNavi = numNavi;
@@ -48,10 +50,10 @@ public class Giocatore {
         return direzione;
     }
 
-    public void popolaMappa(Casella[][] mappa, int DIM){
-        for(int i = 0; i < DIM; i++){
-            for (int j = 0; j < DIM; j++){
-                mappa[i][j]=new Casella('~');
+    public void popolaMappa(Casella[][] mappa, int DIM) {
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                mappa[i][j] = new Casella('~');
             }
         }
     }
@@ -71,22 +73,41 @@ public class Giocatore {
         System.out.print("\n");
     }
 
-    public int scegliPosizione(char direzione, int DIM, int dimNave ,boolean isPosX, Scanner sc) throws IndexOutOfBoundsException {
+    public int scegliPosizione(char direzione, int DIM, int dimNave, boolean isPosX, Scanner sc) throws IndexOutOfBoundsException {
         int posizione;
         posizione = sc.nextInt();
-        if (isPosX && (posizione > DIM || posizione < 1 ||direzione == 'o' &&  posizione > DIM-dimNave+1))
+        if (isPosX && (posizione > DIM || posizione < 1 || direzione == 'o' && posizione > DIM - dimNave + 1))
             throw new IndexOutOfBoundsException("index out of bounds");
-        if (!isPosX && ((posizione > DIM || posizione < 1 ||direzione == 'v' &&  posizione > DIM-dimNave+1))){
+        if (!isPosX && ((posizione > DIM || posizione < 1 || direzione == 'v' && posizione > DIM - dimNave + 1))) {
             throw new IndexOutOfBoundsException("index out of bounds");
         }
         return posizione;
 
     }
 
-    /*public boolean attaccaNave(int posX, int posY, Casella[][] mappa){
-        if(mappa[posY][posX] ==  )
-    }*/
+    public boolean hasFoundNave(int posX, int posY, Casella[][] mappa) {
+        return mappa[posY - 1][posX - 1].getContenuto() == '+';
+    }
 
+    public void mostraMappa(Casella[][] mappa, int DIM) {
+        System.out.print("\t");
+        for (int i = 0; i < DIM; i++)
+            System.out.print(i + 1 + "\t");
+        for (int i = 0; i < DIM; i++) {
+            //stampa y
+            System.out.print("\n" + (i + 1) + "\t");
+            //stampa x
+            for (int j = 0; j < DIM; j++) {
+                if (mappa[i][j].isShown()) {
+                    System.out.print(mappa[i][j].getContenuto() + "\t");
+                }
+                else{
+                    System.out.print("?\t");
+                }
+            }
+        }
+        System.out.print("\n");
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -17,21 +17,25 @@ public class Main {
                 sc.nextLine();
                 flag = false;
             }
-        } while(!flag || DIM_MAPPA < 5 || DIM_MAPPA > 7);
+        } while (!flag || DIM_MAPPA < 5 || DIM_MAPPA > 7);
 
         Casella[][] mappa;
-        Giocatore giocatore = new Giocatore("dummy", DIM_MAPPA - 3);
-        giocatore.inizializzaNavi();
+        Giocatore giocatore1 = new Giocatore("dummy", DIM_MAPPA - 3);
+        Giocatore giocatore2 = new Giocatore("dummy2", DIM_MAPPA - 3);
+        giocatore1.inizializzaNavi();
         mappa = new Casella[DIM_MAPPA][DIM_MAPPA];
 
-        giocatore.popolaMappa(mappa, DIM_MAPPA);
-        giocatore.visualizzaMappa(mappa, DIM_MAPPA);
-        for (int i = 0; i < giocatore.getNumNavi(); i++) {
+        giocatore1.popolaMappa(mappa, DIM_MAPPA);
+        giocatore2.popolaMappa(mappa, DIM_MAPPA);
+        giocatore1.visualizzaMappa(mappa, DIM_MAPPA);
+
+        //inserisci nave
+        for (int i = 0; i < giocatore1.getNumNavi(); i++) {
             //direction
             do {
                 flag = true;
                 try {
-                    giocatore.getNavi()[i].setDirezione(giocatore.scegliDirezione());
+                    giocatore1.getNavi()[i].setDirezione(giocatore1.scegliDirezione());
                 } catch (InstantiationError e) {
                     System.out.println("Direction must be either 'o' or 'v' ");
                     flag = false;
@@ -46,7 +50,7 @@ public class Main {
                     do {
                         flag = true;
                         try {
-                            giocatore.getNavi()[i].setPosX(giocatore.scegliPosizione(giocatore.getNavi()[i].getDirezione(), DIM_MAPPA, giocatore.getNavi()[i].getDimensione(), true, sc));
+                            giocatore1.getNavi()[i].setPosX(giocatore1.scegliPosizione(giocatore1.getNavi()[i].getDirezione(), DIM_MAPPA, giocatore1.getNavi()[i].getDimensione(), true, sc));
                         } catch (IndexOutOfBoundsException e) {
                             System.out.println("la nave non rientra nei limiti della mappa");
                             flag = false;
@@ -61,7 +65,7 @@ public class Main {
                     System.out.println("Inserisci la posizione Y");
 
                     try {
-                        giocatore.getNavi()[i].setPosY(giocatore.scegliPosizione(giocatore.getNavi()[i].getDirezione(), DIM_MAPPA, giocatore.getNavi()[i].getDimensione(), false, sc));
+                        giocatore1.getNavi()[i].setPosY(giocatore1.scegliPosizione(giocatore1.getNavi()[i].getDirezione(), DIM_MAPPA, giocatore1.getNavi()[i].getDimensione(), false, sc));
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("la nave non rientra nei limiti della mappa");
                         flag = false;
@@ -73,17 +77,63 @@ public class Main {
                 } while (!flag);
 
                 try {
-                    giocatore.getNavi()[i].isFree(mappa);
+                    giocatore1.getNavi()[i].isFree(mappa);
                 } catch (OccupiedSlotException e) {
                     System.out.println("la nave interseca con un'altra");
                     flag = false;
                 }
             } while (!flag);
 
-            giocatore.getNavi()[i].insert(mappa);
-            giocatore.visualizzaMappa(mappa, DIM_MAPPA);
-
+            giocatore1.getNavi()[i].insert(mappa);
+            giocatore1.visualizzaMappa(mappa, DIM_MAPPA);
         }
+
+        //attaca nave
+
+        do {
+            giocatore1.mostraMappa(mappa,DIM_MAPPA);
+            flag = true;
+            int indovinaPosX = 0;
+            int indovinaPosY = 0;
+
+            do {
+                flag = true;
+                System.out.println(giocatore2.getNick() + " inserire la x... ");
+                try {
+                    indovinaPosX = sc.nextInt();
+                } catch (Exception InputMismatchException) {
+                    sc.nextLine();
+                    System.out.println("Il numero deve essere intero");
+                    flag = false;
+                }
+            } while (!flag || indovinaPosX > DIM_MAPPA || indovinaPosX < 1);
+            do {
+                flag = true;
+                System.out.println("...e la y di dove si vuole provare ad attaccare");
+                try {
+                    indovinaPosY = sc.nextInt();
+                } catch (Exception InputMismatchException) {
+                    sc.nextLine();
+                    System.out.println("Il numero deve essere intero");
+                    flag = false;
+                }
+            } while (!flag || indovinaPosY > DIM_MAPPA || indovinaPosY < 0);
+            mappa[indovinaPosY-1][indovinaPosX-1].setShown(true);
+            if (giocatore1.hasFoundNave(indovinaPosX, indovinaPosY, mappa)){
+                mappa[indovinaPosY-1][indovinaPosX-1].setContenuto('x');
+                System.out.println("nave affondata");
+            }
+            else{
+                System.out.println("colpo a vuoto");
+            }
+
+                for (int i = 0; i < DIM_MAPPA; i++) {
+                    for (int j = 0; j < DIM_MAPPA; j++)
+                        if (mappa[i][j].getContenuto() == '+') {
+                            flag = false;
+                        }
+                }
+        } while (!flag);
 
     }
 }
